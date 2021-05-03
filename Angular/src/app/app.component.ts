111fcf275529c518a16cd4c5da30dbaf697ec1ce
Alex Shen <shen.alexander@gmail.com>
2021-05-03 01:55:43 -0400

    Refactored out data access so we don't do it 7 times

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -8,8 +10,32 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'FridginCool';
 
+  session : string | undefined;
+
   week = [
    "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
   ];
 
+  meals : string[] | undefined; // To be filled with user's saved meals from database
+
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
+    this.route.queryParams
+      .subscribe(params => {
+          this.session = params.session;
+          console.log(this.session)
+
+          const headers = {'Content-Type': 'application/json; charset=utf-8'};
+          const body = {'session': this.session};
+          this.http.post('http://localhost/FridginCool/PHP/planner-backend.php',
+            body, {responseType: "text"})
+            .subscribe(response => {
+              console.log(response);
+              this.meals = response.replace(/(\r\n|\n|\r)/gm, "").split(",")
+              console.log(this.meals)
+            }, error => {
+              console.log('ERROR: ', error);
+            });
+        }
+      );
+  }
 }
--- Angular/src/app/day/day.component.ts
+++ Angular/src/app/day/day.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {ActivatedRoute} from "@angular/router";
+
 
 @Component({
   selector: 'app-day',
@@ -8,50 +7,10 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./day.component.css']
 })
 export class DayComponent implements OnInit{
-  session : string | undefined;
-
-  constructor(private http: HttpClient, private route: ActivatedRoute) {
-
-  }
-
   @Input() dayOfWeek: any;
-
-  week = [
-    'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
-  ]; // Unused, just for reference
-
-  // Sample meals
-  meals = [
-    'Mac & Cheese',
-    'Mashed Potatoes',
-    'Grilled Chicken',
-    'Garden Salad',
-    'Baked Beans',
-    'Ice Cream',
-    'Mushroom Soup',
-    'Pizza'
-  ];// To be replaced by user's saved meals from database
+  @Input() meals : any;
 
   ngOnInit(): void {
-    this.route.queryParams
-      .subscribe(params => {
-          this.session = params.session;
-          console.log(this.session)
 
-        const headers = {'Content-Type': 'application/json; charset=utf-8'};
-        const body = {'session': this.session};
-        this.http.post('http://localhost/FridginCool/PHP/planner-backend.php',
-          body, {responseType: "text"})
-          .subscribe(response => {
-            console.log(response);
-            this.meals = response.replace(/(\r\n|\n|\r)/gm, "").split(",")
-            console.log(this.meals)
-          }, error => {
-            console.log('ERROR: ', error);
-          });
-        }
-      );
   }
-
-
 }
