f2b4be1e448cd2ec47c2d8558666ccb642e60d8e
Alex Shen <shen.alexander@gmail.com>
2021-05-03 00:59:54 -0400

    Loads user's meals into planner

--- Angular/src/app/day/day.component.ts
+++ Angular/src/app/day/day.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {ActivatedRoute} from "@angular/router";
 
 @Component({
@@ -37,19 +37,20 @@ export class DayComponent implements OnInit{
       .subscribe(params => {
           this.session = params.session;
           console.log(this.session)
+
+        const headers = {'Content-Type': 'application/json; charset=utf-8'};
+        const body = {'session': this.session};
+        this.http.post('http://localhost/FridginCool/PHP/planner-backend.php',
+          body, {responseType: "text"})
+          .subscribe(response => {
+            console.log(response);
+            this.meals = response.replace(/(\r\n|\n|\r)/gm, "").split(",")
+            console.log(this.meals)
+          }, error => {
+            console.log('ERROR: ', error);
+          });
         }
       );
-
-    const headers = {'Content-Type': 'application/json; charset=utf-8'};
-    const body = {'email': 'root@test.com', 'session': this.session};
-    this.http.post('http://localhost/FridginCool/PHP/planner-backend.php',
-      body, {headers, responseType: 'text'}, )
-      .subscribe(response => {
-        console.log(response);
-      }, error => {
-        console.log('ERROR: ', error);
-      });
-
   }
 
 
--- PHP/planner-backend.php
+++ PHP/planner-backend.php
@@ -1,37 +1,51 @@
 <?php
-if(isset($_POST['session'])) {
-    session_id($_POST['session']);
-    echo "hi";
-}
-session_start();
-require('connect-db.php');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding');
 header('Access-Control-Max-Age: 1000');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT');
-// Angular frontend should make a POST request to this endpoint with the session ID
-echo session_id() . "\n";
-$email = "*";
-if(isset($_SESSION['email'])){
+
+$json = file_get_contents('php://input');
+
+// Converts it into a PHP object
+$postdata = json_decode($json);
+
+$data = [];
+foreach ($postdata as $field) {
+    array_push($data, $field);
+}
+
+//$email = $data[0];
+$session = $data[0];
+
+//echo $email;
+//echo $session;
+
+session_id($session);
+session_start();
+
+require('connect-db.php');
+if (isset($_SESSION['email'])) {
     $email = $_SESSION['email'];
-}
 
-//echo "email is " . $email ;
-global $db; // Name needs to match connect-db.php
-//$query = "SELECT * FROM meals WHERE user_email = :user_email";
-$query = "SELECT * FROM meals";//" WHERE user_email = :user_email";
-$statement = $db->prepare($query);
-$statement->bindValue(':user_email', $email);
-$statement->execute();
+    //echo "email is " . $email ;
+    global $db; // Name needs to match connect-db.php
+    $query = "SELECT * FROM meals WHERE user_email = :user_email";
+//    $query = "SELECT * FROM meals";//" WHERE user_email = :user_email";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':user_email', $email);
+    $statement->execute();
 
-$results = $statement->fetchAll();
+    $results = $statement->fetchAll();
 
-$statement->closeCursor();
+    $statement->closeCursor();
 
-$meal_titles = [];
-foreach ($results as $result){
-    array_push($meal_titles, $result['title']);
-}
+    $meal_titles = [];
+    foreach ($results as $result) {
+        array_push($meal_titles, $result['title']);
+    }
 
 // combines all titles into comma-separated string
-echo implode(',',$meal_titles);
+    echo implode(',', $meal_titles);
+}
+
+
