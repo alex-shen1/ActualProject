de27df7d3de6034b6a371de39cd5a210016a23fa
Alex Shen <shen.alexander@gmail.com>
2021-05-03 20:04:18 -0400

    Fix to allow CORS on localhost

--- PHP/planner-backend.php
+++ PHP/planner-backend.php
@@ -1,8 +1,8 @@
 <?php
-//header('Access-Control-Allow-Origin: *');
-//header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding');
-//header('Access-Control-Max-Age: 1000');
-//header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT');
 
 $json = file_get_contents('php://input');
 
