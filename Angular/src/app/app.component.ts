a440a16875a1f3ace0a67e9fd92c0d22a4123eeb
Alex Shen <shen.alexander@gmail.com>
2021-05-03 21:20:34 -0400

    Add comments explaining app.component.ts

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -23,20 +23,30 @@ export class AppComponent {
   constructor(private http: HttpClient, private route: ActivatedRoute) {
     this.route.queryParams
       .subscribe(params => {
+          // use subscribe to access parameters in the URL, which session ID is the only one
           this.session = params.session;
           console.log(this.session)
 
+          // prepare a POST request to PHP backend getting meal plan for the current session's user
           const headers = {'Content-Type': 'application/json; charset=utf-8'};
           const body = {'session': this.session};
           this.http.post('http://localhost/FridginCool/PHP/planner-backend.php',
             body, {responseType: "text"})
             .subscribe(response => {
-              // console.log(response);
+              // returns JSON of mealnames as a comma-separated string and a data structure
+              // holding the schedules for each day e.g. "Monday" corresponds to a dictionary
+              // mapping "Breakfast", "Lunch", etc. to string names of the meal for that time
+              // will be "None" if database didn't have an entry for that day/time
+
+              // formatting done in the PHP, we just need to use it
               const response_json = JSON.parse(response);
               console.log(response_json)
               const mealnames = response_json['mealnames'];
+              // comma-separated string with meal names
               this.meals = mealnames.replace(/(\r\n|\n|\r)/gm, "").split(",")
               // console.log(this.meals)
+
+              // meal plan data structure
               this.mealplan = response_json['plan'];
               console.log(this.mealplan)
             }, error => {
